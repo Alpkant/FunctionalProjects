99a3848f622606235c4a7d7de517b1691213c583
Alpkant <kantarcia@itu.edu.tr>
2018-02-17 12:22:41 +0300

    Function return range changed from 0..6 to 1..7 according to the python function

--- Project1/Deneme.hs
+++ Project1/Deneme.hs
@@ -1,21 +1,18 @@
 -- Function takes year month and day returns day as integer.
--- Starts with Saturday = 0 and continue until Friday = 6       
+-- Starts with Saturday = 1 and continue until Friday = 7       
 dayOfWeek :: Integer -> Integer -> Integer -> Integer
 dayOfWeek year month day  = mod ( day + t + k + floor(fromIntegral k / 4) + floor(fromIntegral j / 4) + (5*j) ) 7 
     where 
         month' 
-         | month == 1 = 13
-         | month == 2 = 14 
-
-        year' 
-         | month == 1 || month == 2 = year - 1
+         | month <= 2 = month + 12
 
         t :: Integer
         t = floor (fromIntegral (13 * (month' + 1)) / 5.0)
 
         k :: Integer
-        k  = mod year' 100
+        k  = mod year 100
 
         j :: Integer
-        j  = div year' 100
+        j  = div year 100
   
+
