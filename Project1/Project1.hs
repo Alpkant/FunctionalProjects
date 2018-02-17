2b44e44ea0436fdc70d63cd90f5739098f7a71d3
Alpkant <kantarcia@itu.edu.tr>
2018-02-17 13:55:38 +0300

    Question 2 answers and codes added.

--- Project1/Project1.hs
+++ Project1/Project1.hs
@@ -1,19 +1,33 @@
--- Function takes year month and day returns day as integer.
--- Starts with Saturday = 1 and continue until Friday = 7       
-dayOfWeek :: Integer -> Integer -> Integer -> Integer
-dayOfWeek year month day  = mod ( day + t + k + floor(fromIntegral k / 4) + floor(fromIntegral j / 4) + (5*j) ) 7 
-    where 
-        month' 
-         | month <= 2 = month + 12
-         | otherwise = month
+sundays1 :: Integer -> Integer -> Integer
+sundays1 start end = sundays' start 1
+  where
+    sundays' :: Integer -> Integer -> Integer
+    sundays' y m
+      | y > end = rest
+      | otherwise = if dayOfWeek y m 1 == 1 then rest + 1 else rest
+        where
+          nextY = if m == 12 then y + 1 else y
+          nextM = if m == 12 then 1 else m + 1 
+          rest = if y > end then 0 else sundays' nextY nextM
+    dayOfWeek :: Integer -> Integer -> Integer -> Integer
+    dayOfWeek year month day  = mod ( day + t + k + floor(fromIntegral k / 4) + floor(fromIntegral j / 4) + (5*j) ) 7 
+      -- Helper function takes year month and day returns day as integer.
+      -- Starts with Saturday = 1 and continue until Friday = 7         
+      where 
+          month' 
+           | month <= 2 = month + 12
+           | otherwise = month
 
-        t :: Integer
-        t = floor (fromIntegral (13 * (month' + 1)) / 5.0)
+          t :: Integer
+          t = floor (fromIntegral (13 * (month' + 1)) / 5.0)
+  
+          k :: Integer
+          k  = mod year 100
+  
+          j :: Integer
+          j  = div year 100 
 
-        k :: Integer
-        k  = mod year 100
+       
 
-        j :: Integer
-        j  = div year 100
   
 
