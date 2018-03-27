954b375fc475f4fc1a34e2ecc32d39331977b555
Alpkant <kantarcia@itu.edu.tr>
2018-03-27 11:17:53 +0300

    Question 4 and 5 functions implemented.

--- Project2/Main.hs
+++ Project2/Main.hs
@@ -22,5 +22,25 @@ cardValue card = case rank card of
     Num value -> value
     Ace       -> 10
     _         -> 11
+
+--removeCard :: [Card] -> Card -> [Card]
+--removeCard list current = removeCard' list current False
+--    where
+--        removeCard' :: [Card] -> Card -> Bool -> [Card]
+--        removeCard'
+
+allSameColor :: [Card] -> Bool
+allSameColor [x]        = True
+allSameColor [x1,x2]    = if cardColor x1 == cardColor x2 then True else False
+allSameColor (x1:x2:xs) = if cardColor x1 == cardColor x2 then allSameColor (x2:xs) else False
+
+sumCards :: [Card] -> Int
+sumCards list = sumCards' list 0
+    where
+        sumCards' :: [Card] -> Int -> Int
+        sumCards' [] acc = acc
+        sumCards' (x:xs) acc = sumCards' xs (acc + cardValue x)
+
+
 main :: IO ()
 main = return ()
