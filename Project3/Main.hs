2ef86be58a8114c909ff6a601e31954fbb15515c
Alperen Kantarcı <kantarcia@itu.edu.tr>
2018-04-29 09:55:03 +0300

    sentenceCharCounts function is implemented

--- Project3/Main.hs
+++ Project3/Main.hs
@@ -1,13 +1,16 @@
 import Prelude hiding (Word)
 import Data.Char
 import Data.List
-type Word = String
-type Sentence = [Word]
+type Word       = String
+type Sentence   = [Word]
 type CharCounts = [(Char, Int)]
 
 wordCharCounts :: Word -> CharCounts
 wordCharCounts = map (\x -> (head x,length x)) . group . sort . toLowerWord
-	where
-		toLowerWord ::  Word -> Word
-		toLowerWord  = map toLower 
-		
+    where
+      toLowerWord ::  Word -> Word
+      toLowerWord  = map toLower
+
+sentenceCharCounts :: Sentence -> CharCounts
+sentenceCharCounts [x]      = wordCharCounts x
+sentenceCharCounts (x:xs)   = (wordCharCounts x) ++ (sentenceCharCounts xs)
