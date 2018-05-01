6a3b1ed7e0651be06c1b520e928ad1e6487e1fea
Alperen Kantarcı <kantarcia@itu.edu.tr>
2018-05-01 19:33:53 +0300

    CharCounts and function implemented with Map

--- Project3/Main.hs
+++ Project3/Main.hs
@@ -1,19 +1,21 @@
 import Prelude hiding (Word)
 import Data.Char
 import Data.List
+import qualified Data.Map as M
+
 type Word       = String
 type Sentence   = [Word]
-type CharCounts = [(Char, Int)]
+type CharCounts = M.Map Char Int
 
 wordCharCounts :: Word -> CharCounts
-wordCharCounts = map (\x -> (head x,length x)) . group . sort . toLowerWord
+wordCharCounts = M.fromList . map (\x -> (head x,length x)) . group . sort . toLowerWord
     where
       toLowerWord ::  Word -> Word
       toLowerWord  = map toLower
 
 sentenceCharCounts :: Sentence -> CharCounts
 sentenceCharCounts [x]      = wordCharCounts x
-sentenceCharCounts (x:xs)   = (wordCharCounts x) ++ (sentenceCharCounts xs)
+sentenceCharCounts (x:xs)   = M.unionWith (+) (wordCharCounts x)  (sentenceCharCounts xs)
 
 dictCharCounts :: Sentence -> [(Word,CharCounts)]
 dictCharCounts = map (\x -> (x,wordCharCounts x))
