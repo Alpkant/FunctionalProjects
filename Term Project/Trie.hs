76c66a857f7d27a96d58f68d7f35d0b678f47c1a
Alperen Kantarcı <kantarcia@itu.edu.tr>
2018-05-14 23:12:17 +0300

    basecase of the insert function is fixed

--- Term Project/Trie.hs
+++ Term Project/Trie.hs
@@ -12,7 +12,7 @@ empty :: Trie
 empty = Trie {end = False , children = M.empty }
 
 insert :: Word -> Trie -> Trie
-insert [] (Trie e t)        = Trie {end = True , children = M.empty } --End char will have True for end
+insert [] (Trie e t)        = Trie {end = True , children = t } --End char will have True for end
 insert x'@(x:xs) (Trie e t) = case M.lookup x t of
                               Just value -> Trie {end = e , children = M.insert x (insert xs value) t }
                               Nothing    -> Trie {end = e , children = M.insert x (insert xs empty) t }
